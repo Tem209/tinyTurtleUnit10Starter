1de8f93006e577036adbab4f06a9b00dd56ff3ba
Tembani <tembani437@gmail.com>
2017-03-01 21:20:16 +0000

    we did things here

--- script.js
+++ script.js
@@ -12,7 +12,31 @@ right(90);
 forward(50);
 }
 
+function rectangle(){
+forward(50);
+right(90);
+forward(20);
+right(90);
+forward(50);
+right(90);
+forward(20);
+right(90);
+forward();
+}
+function house(){
+   forward(20);
+right(45);
+forward(20);
+right(90);
+forward(20);
+right(45);
+forward(20);
+right(90);
+forward(40);
+
+}
 // Type your function call below
-
-
+//square();
+//rectangle();
+house();
 stamp();
